0969f1fc16d2d464684a86cb5cefa10f468f3637
Scott Wood <scottw@finao.com>
2015-01-22 17:31:24 -0500

    updated the migrate tool to work from the command line. Updated the index for 'migration' to include instructions on running from the command line.

--- module/Application/src/Application/Controller/MigrationController.php
+++ module/Application/src/Application/Controller/MigrationController.php
@@ -11,7 +11,7 @@ class MigrationController extends AbstractController
     public function indexAction()
     {
         $myActions = $this->getActionNames(array('indexAction'));
-        return new ViewModel(array('actions' => $myActions));
+        return new ViewModel(array('actions' => $myActions, 'usage' => $this->usageMessage()));
     }
 
     public function materialsAction() {
@@ -26,12 +26,13 @@ class MigrationController extends AbstractController
 
     public function cliAction() {
         $mapper = $this->getMapper();
-        $mapper->setsetOutputType('echo');
+        $mapper->setOutputType('echo');
 
         $request = $this->getRequest();
 
         // Check verbose flag
         $verbose = $request->getParam('verbose') || $request->getParam('v');
+        $verbosity = ($verbose) ? 3 : 1;
 
         // Check mode
         $mode = $request->getParam('mode'); // defaults to 'all'
@@ -39,17 +40,31 @@ class MigrationController extends AbstractController
         $users = array();
         switch ($mode) {
             case 'materials':
-                $output = $mapper->migrateJoomlaColors(2);
+                $output = $mapper->migrateJoomlaColors($verbosity);
                 break;
             case 'collections':
-                $users = $mapper->migrateJoomlaMaterials(2);
+                $output = $mapper->migrateJoomlaMaterials($verbosity);
                 break;
             case 'colorrefs':
-                $users = $mapper->migrateColorRefs(2);
+                $output = $mapper->migrateColorRefs($verbosity);
                 break;
+            default:
+                echo $this->usageMessage();
+                break;
+
         }
     }
 
+    private function usageMessage() {
+        return <<<EOT
+
+  Usage:
+         php public/index.php [--verbose|-v]
+               migration [materials|collections|colorrefs]:mode
+
+
+EOT;
+    }
     public function migmatAction() {
 
         $mapper = $this->getMapper();
